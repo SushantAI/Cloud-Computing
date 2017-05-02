ca7aa8ab8dba5fc9c8a317626d65b70c3a48a696
SushantChatufale <sushantchatufale@gmail.com>
2017-05-02 10:55:38 +0100

    Updated 1

--- src/MyWebSocketEndPoint.java
+++ src/MyWebSocketEndPoint.java
@@ -20,46 +20,46 @@ public class MyWebSocketEndPoint {
 	private static Map<String, Session> persons = Collections.synchronizedMap(new LinkedHashMap<String, Session>());
 	
 	@OnOpen
-	public void onOpen(Session session) throws Exception {
+	public void onOpen(Session websocOpen) throws Exception {
 
 	    //Get the new socket's username from url
 	    //e.g. url: ws://localhost:8080/chat?username=Andi, so Andi is the username
-	    Map<String, List<String>> parameter = session.getRequestParameterMap();
-	    List<String> list = parameter.get("person");
-	    String addPerson = list.get(0);
+	    Map<String, List<String>> attribute = websocOpen.getRequestParameterMap();
+	    List<String> lsPrsn = attribute.get("person");
+	    String addPerson = lsPrsn.get(0);
 	    System.out.println(addPerson);
 	    //Add the new socket to the collection
-	    persons.put(addPerson, session);
+	    persons.put(addPerson, websocOpen);
 
 	    //also set username property of the session.
 	    //so when there a new message from a particular socket's session obj
 	    //we can get the username whom send the message
-	    session.getUserProperties().put("person", addPerson);
+	    websocOpen.getUserProperties().put("person", addPerson);
 
 	    //Give a list current online users to the new socket connection
 	    //because we store username as the key of the map, we can get all
 	    //  username list from the map's keySet
 	    String reply = "newUser|" + String.join("|", persons.keySet());
-	    session.getBasicRemote().sendText(reply);
+	    websocOpen.getBasicRemote().sendText(reply);
 
 	    //Loop through all socket's session obj, then send a text message
 	    for (Session slave : persons.values()) {
-	        if(slave == session) continue;
+	        if(slave == websocOpen) continue;
 	        slave.getBasicRemote().sendText("newUser|" + addPerson);
 	    }
 	}
 
 	@OnMessage
-	public void onMessage(Session session, String message) throws Exception {
+	public void onMessage(Session websocOnMsg, String chunk) throws Exception {
 	    //Extract the information of incoming message.
 	    //Message format: 'From|Message'
 	    //so we split the incoming message with '|' token
 	    //to get the destination and message content data
-	    String[] msg = message.split("\\|");
+	    String[] msg = chunk.split("\\|");
 	    String receiver = msg[0]; String msgBody = msg[1];
 
 	    //Retrieve the sender's username from it's property
-	    String sender = (String)session.getUserProperties().get("person");
+	    String getPerson = (String)websocOnMsg.getUserProperties().get("person");
 
 	    //Deliver the message according to the destination
 	    //Outgoing Message format: "message|sender|content|messageType?"
@@ -68,26 +68,26 @@ public class MyWebSocketEndPoint {
 	    if(receiver.equals("all")) {
 	        //if the destination chat is 'all', then we broadcast the message
 	        for (Session client : persons.values()) {
-	            if(client.equals(session)) continue;
-	            client.getBasicRemote().sendText("message|" + sender + "|" + msgBody + "|all" );
+	            if(client.equals(websocOnMsg)) continue;
+	            client.getBasicRemote().sendText("message|" + getPerson + "|" + msgBody + "|all" );
 	        }
 	    } else {
 	        //find the username to be sent, then deliver the message
 	        Session slave = persons.get(receiver);
-	        String reply = "message|" + sender + "|" + msgBody;
+	        String reply = "message|" + getPerson + "|" + msgBody;
 	        slave.getBasicRemote().sendText(reply);
 	    }
 	}
 
 	@OnClose
-	public void onClose(Session session) throws Exception {
+	public void onClose(Session websocClose) throws Exception {
 	    //remove client from collecton  
-	    String username = (String)session.getUserProperties().get("person");
-	    persons.remove(username);
+	    String persn = (String)websocClose.getUserProperties().get("person");
+	    persons.remove(persn);
 	    
 	    //broadcast to all people, that the current user is leaving the chat room
 	    for (Session slave : persons.values()) {
-	        slave.getBasicRemote().sendText("removeUser|" + username);
+	        slave.getBasicRemote().sendText("removeUser|" + persn);
 	    }
 	}
 }
